4cc6f6800fc497b247b82deef26ccd61ed3221e9
Jinyang88 <liujy725@gmail.com>
2018-03-31 11:40:59 +0800

    add Prediction

--- src/lstm_match.py
+++ src/lstm_match.py
@@ -18,7 +18,7 @@ import datahelper
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 
 
-test_mode = 0  # 0 for train+test 1 for test
+test_mode = 1  # 0 for train+test 1 for test
 device = -1 # 0 for gpu, -1 for cpu
 
 bidirectional = True
@@ -56,12 +56,12 @@ print('Building vocabulary Finished.')
 
 
 train_iter = data.BucketIterator(dataset=train, batch_size=batch_size, sort_key=lambda x: len(x.Text), device=device, repeat=False)
-valid_iter = data.Iterator(dataset=valid, batch_size=batch_size, device=device, shuffle=False, repeat=False)
-test_iter = data.Iterator(dataset=test, batch_size=batch_size, device=device, shuffle=False, repeat=False)
+valid_iter = data.Iterator(dataset=valid, batch_size=len(valid.examples), device=device, shuffle=False, repeat=False)
+test_iter = data.Iterator(dataset=test, batch_size=len(test.examples), device=device, shuffle=False, repeat=False)
 
 train_dl = datahelper.BatchWrapper(train_iter, ["Text", "Emoji", "Label"])
-valid_dl = datahelper.BatchWrapper(valid_iter, ["Text", "Emoji", "Label"])
-test_dl = datahelper.BatchWrapper(test_iter, ["Text", "Emoji", "Label"])
+valid_dl = datahelper.BatchWrapper(valid_iter, ["Id", "Text", "Emoji", "Label"])
+test_dl = datahelper.BatchWrapper(test_iter, ["Id", "Text", "Emoji", "Label"])
 print('Reading data done.')
 
 
@@ -98,6 +98,30 @@ class lstm_match(torch.nn.Module) :
         return (Variable(torch.randn(batch_size, batch_size, self.hidden_dim)),Variable(torch.randn(1, batch_size, self.hidden_dim)))  
 
 
+# data_dl: id, text, emoji, label
+def predict_on(model, data_dl, model_state_path=None):
+    if model_state_path:
+        model.load_state_dict(torch.load(model_state_path))
+        print('Start predicting...')
+
+    model = MODEL.eval()
+    result_list = []  # id, emoji, similarity, label
+    for ids, text, emoji, label in data_dl:
+        seq_embedding, emoji_embedding = MODEL(text, emoji.view(-1,1), None)
+        p_pred = F.cosine_similarity(seq_embedding.squeeze(1), emoji_embedding.squeeze(1))
+        loss = loss_func(seq_embedding.squeeze(1), emoji_embedding.squeeze(1), label.view(-1,1))
+        result_nparr = ids.data.cpu().numpy(), emoji.data.cpu().numpy(), p_pred.data.cpu().numpy(), label.data.cpu().numpy()
+    result_df = pd.DataFrame()
+    result_df['id'] = result_nparr[0]
+    result_df['emoji'] = result_nparr[1]
+    result_df['similarity'] = result_nparr[2]
+    result_df['label'] = result_nparr[3]
+    answer_df = result_df.loc[result_df.groupby("id")['similarity'].idxmax().values][['id','emoji']].rename(columns={"emoji":"prediction"})
+    ground_truth_df = result_df[result_df['label']==1].rename(columns={"emoji":"groundtruth"})
+    final_df = answer_df.merge(ground_truth_df, on="id")
+    return loss, accuracy_score(final_df['prediction'], final_df['groundtruth'])
+
+
 
 print('Initialing model..')
 MODEL = lstm_match(len(TEXT.vocab),emoji_num, embedding_dim,
@@ -126,12 +150,11 @@ if not test_mode:
             MODEL.zero_grad()
             batch_count += 1
             if batch_count % print_every == 0:
-                loss = 0
-                for text, emoji, label in valid_dl:
-                    MODEL = MODEL.train(False)
-                    seq_embedding, emoji_embedding = MODEL(text, emoji.view(-1,1), None)
-                    loss += loss_func(seq_embedding.squeeze(1), emoji_embedding.squeeze(1), label.view(-1,1))
+                loss, acc = predict_on(MODEL, valid_dl)
                 batch_end = time.time()
                 MODEL = MODEL.train(True)
-                print('Finish {}/{} batch, {}/{} epoch. Time consuming {}s. Loss is {}'.format(batch_count, batch_num, i+1, epochs, round(batch_end - batch_start, 2) ,float(loss)))
-        torch.save(MODEL.state_dict(), 'model' + str(i+1)+'.pth')           
+                print('Finish {}/{} batch, {}/{} epoch. Time consuming {}s. Acc is {}, Loss is {}'.format(batch_count, batch_num, i+1, epochs, round(batch_end - batch_start, 2), acc, float(loss)))
+            torch.save(MODEL.state_dict(), 'model' + str(i+1)+'.pth')           
+            print("Saving model..")
+
+predict_on(MODEL, test_dl, 'model{}.pth'.format(epochs))

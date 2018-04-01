6d5d3d93c667a5fbe7210addbb4b0b184f169cf2
Jinyang88 <liujy725@gmail.com>
2018-04-01 13:01:33 +0800

    fix cuda bug

--- src/BLSTM-MW.py
+++ src/BLSTM-MW.py
@@ -111,8 +111,8 @@ class lstm_match_w(torch.nn.Module) :
         self.hidden_dim = hidden_dim
         
         self.word_embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.word_embedding.weight.data.copy_(wordvec_matrix)
-        self.word_embedding.weight.requires_grad = False
+        # self.word_embedding.weight.data.copy_(wordvec_matrix)
+        # self.word_embedding.weight.requires_grad = False
         
         self.emoji_embedding = nn.Embedding(emoji_num, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim//2 if bidirectional else hidden_dim, batch_first=True, bidirectional=bidirectional, dropout=dropout)
